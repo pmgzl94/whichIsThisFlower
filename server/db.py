65f56bf935c6664bb9d74ed363de7110b1acf7cd
aurele <aurele.auboin@epitech.eu>
2020-11-29 18:25:57 +0100

    [Add]: debug mod for db

--- server/db.py
+++ server/db.py
@@ -10,24 +10,43 @@ class dbManager():
         self.db = self.client.db
         self.collection = self.db.user
         self.posts = self.db.posts
+        self.debug = debug
 
 
 
     def addUser(self, userName, password):
+        if self.debug == True:
+            print("[DBMANAGER]: [addUser]: user : {}, password : {} ".format(userName, password), file=sys.stderr)
         doc = {"username": userName,
                "password": password}
         post_id = self.posts.insert_one(doc).inserted_id
+        if self.debug == True:
+            print("[DBMANAGER]: [addUser]: id : {}".format(post_id), file=sys.stderr)
+            print("[DBMANAGER]: [addUser]: END", file=sys.stderr)
 
     def getUser(self, name):
+        if self.debug == True:
+            print("[DBMANAGER]: [getUsers]: user : {}".format(name), file=sys.stderr)
+
         user = self.posts.find_one({"username": name})
+        if self.debug == True:
+            print (user, file=sys.stderr)
+            print("[DBMANAGER]: [getUsers]: END")
         return user
 
 
     def getNbUser(self):
+        if self.debug == True:
+            print("[DBMANAGER]: [getUsers]", file=sys.stderr)
+
         nb = self.posts.count_documents({})
+        if self.debug == True:
+            print (nb)
+            print("[DBMANAGER]: [getUsers]: END", file=sys.stderr)
         return nb
-        
-dbMan = dbManager("localhost", 27017)
+
+# dbMan = dbManager("localhost", 27017)
+dbMan = dbManager("localhost", 27017, True)
 
 
 
