fbe9c9d5ebfe82655f2d08a3d93d566b5beacd15
gonzalo <pierremickael.gonzalo@gmail.com>
2020-12-18 21:41:56 +0100

    [Fix]: tensor loading

--- server/cnn/fcnetwork.py
+++ server/cnn/fcnetwork.py
@@ -5,7 +5,8 @@ import os
 # class Forward():
 
 def sigmoid(z):
-    return math.exp(z)/(1 + math.exp(z))
+    print(f"z = {z.shape}")
+    return np.exp(z)/(1 + np.exp(z))
 
 class FCNetwork():
     def __init__(self, arch, transfer_learning=None):
@@ -22,33 +23,49 @@ class FCNetwork():
         self.inputshape = (1, arch[0])
         self.depth = len(arch)
     
-    def __init__(self, transfer_learning_file):
+    def __init__(self, depth, transfer_learning_file):
         # + .bs + .ws
         dir = "./tensorfiles"
         print(transfer_learning_file)
-        if not os.path.exists(dir + "/" + transfer_learning_file + ".bs.npy"):
+        if not os.path.exists(dir + "/" + transfer_learning_file + ".ws1.npy"):
             raise Exception("tensor file not exists")
-        self.weights = TensorFileManager("./tensorfiles").load(transfer_learning_file + ".ws")
-        self.biases = TensorFileManager("./tensorfiles").load(transfer_learning_file + ".bs")
-
-        print(self.weights[0].shape)
-        print(self.weights)
-        if len(self.weights.shape) != 1:
-            self.inputshape = (1, self.weights[0].shape[0])
-        else:
-            self.inputshape = (1, self.weights.shape[0])
-        print(self.inputshape)
-        self.depth = len(self.weights) + 1
-
+        
+        self.weights = []
+        self.biases = []
+        self.depth = depth
+        for i in range(0, depth):
+            pathw = f"{transfer_learning_file}.ws{i + 1}"
+            pathb = f"{transfer_learning_file}.bs{i + 1}"
+            w = TensorFileManager("./tensorfiles").load(pathw)
+            b = TensorFileManager("./tensorfiles").load(pathb)
+            self.weights.append(w)
+            self.biases.append(w)
+        self.inputshape = (self.weights[0].shape[1],)
 
     def compute(self, input):
         # print(input.shape)
+        print(input.shape)
+        print(self.inputshape)
+        # print(len(input.shape))
+
+        # if len(input.shape) == 1 and input.shape[0] == self.inputshape[1]:
+        #     input = input.reshape(1, input.shape[0])
         if input.shape != self.inputshape:
             raise Exception("wrong input shape")
+        print(f'input shape after checking = {input.shape}')
+        print(f'b shape = {self.biases}')
+        print(f'w shape = {self.weights}')
         return self.forward(input)
 
     def forward(self, input):
         x = input
-        for i in range(0, self.depth-1):
-            x = sigmoid(np.dot(self.weights[i], x) + self.biases[i])
+        print(self.weights[0].shape)
+        print(self.weights[1].shape)
+        for i in range(0, self.depth):
+            print("1st it")
+            # print(self.weights[)
+            # print(self.weights.)
+            # x = sigmoid(np.dot(self.weights.T, x) + self.biases[i])
+            x = sigmoid(np.dot(x, self.weights[i].T) + self.biases[i])
+            print(x)
         return x
--- server/cnn/fcnetwork.test.py
+++ server/cnn/fcnetwork.test.py
@@ -5,21 +5,48 @@ from tensor.Tensor import TensorFileManager
 import os
 
 class TestSum(unittest.TestCase):
-    def test_perceptron1(self):
-        """
-        Test that it can sum a list of integers
-        """
+    #### forward test
+    # def test_perceptron1(self):
+    #     """
+    #     Test that it can sum a list of integers
+    #     """
+    #     pathdir = "./tensorfiles"
+    #     filename = "perceptest1"
+    #     fullypath = pathdir + "/" + filename
+    #     if not os.path.isdir(os.path.join(pathdir, filename + ".bs.npy")):
+    #         TensorFileManager("./tensorfiles").save(filename + ".bs", numpy.array([0.4]))
+    #         TensorFileManager("./tensorfiles").save(filename + ".ws", numpy.array([1.1]))
+    #     net = fcn.FCNetwork(filename)
+    #     expected_res = fcn.sigmoid(0.4 + 1.1)
+    #     res = net.compute(numpy.array([1]).reshape(1, 1))
+
+    #     self.assertEqual(res, expected_res)
+
+    ##test with a bigger network
+    def test_shallow_network1(self):
         pathdir = "./tensorfiles"
-        filename = "perceptest1"
+        filename = "shallow_network1"
         fullypath = pathdir + "/" + filename
-        if not os.path.isdir(os.path.join(pathdir, filename + ".bs.npy")):
-            TensorFileManager("./tensorfiles").save(filename + ".bs", numpy.array([0.4]))
-            TensorFileManager("./tensorfiles").save(filename + ".ws", numpy.array([1.1]))
-        net = fcn.FCNetwork(filename)
-        expected_res = fcn.sigmoid(0.4 + 1.1)
-        res = net.compute(numpy.array([1]).reshape(1, 1))
+        if not os.path.isdir(os.path.join(pathdir, filename + ".bs1.npy")):
+            print("la")
+            # ws = [[numpy.array([1, 0.7]), numpy.array([0.1, 0.8]), numpy.array([0.4, 0.9])], [numpy.array([0.9, 0.7, 0.1])]]
+            ws1 = numpy.array([[1, 0.7], [0.1, 0.8], [0.4, 0.9]])
+            ws2 = numpy.array([0.9, 0.7, 0.1])
+            ws  = [ws1, ws2]
+            bs = [[0, 0, 0]]
+            # return
+            TensorFileManager("./tensorfiles").save(filename + ".bs1", bs)
+            TensorFileManager("./tensorfiles").save(filename + ".bs2", bs)
+            TensorFileManager("./tensorfiles").save(filename + ".ws1", ws1)
+            TensorFileManager("./tensorfiles").save(filename + ".ws2", ws2)
+        net = fcn.FCNetwork(depth=2, transfer_learning_file=filename)
+
+        input = numpy.array([0.3, 0.4])
+        expected_res = 0.7406534729647368
+        res = net.compute(input)
 
         self.assertEqual(res, expected_res)
+
 
     # def test_list_fraction(self):
     #     """
