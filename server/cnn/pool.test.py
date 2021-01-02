934a60f8ae93ce020fcc7519753d1b8b93f75b18
gonzalo <pierremickael.gonzalo@gmail.com>
2021-01-02 16:10:51 +0100

    [Add]: test for max pool build derivative, it miss to test the backprop helper

--- server/cnn/pool.test.py
+++ server/cnn/pool.test.py
@@ -24,6 +24,24 @@ class TestFcNetwork(unittest.TestCase):
         cmp = (output == expected_res)
         
         self.assertEqual(cmp.all(), True)
+    
+    def test_derivative_with_depth3(self):
+        input = numpy.array([[[1, 2, 11, 0.3], [0, 4, 4, 36], [0, 12, 27, 34], [62, 12, 11, 10]],
+                             [[11, 58, -1, 2], [1, 36, 12, 8], [27, 0, 64, 1], [1, -1, 4, 3]],
+                            [[1, 3, 24, 5], [12, 2, 0, 1], [-1, 2, 3, 11], [12, 27, 18, 2]]])
+        
+        p = pool.PoolLayer()
+        p.compute(input)
+
+        expected_res = numpy.array([[[0, 0, 0, 0], [0, 1, 0, 1], [0, 0, 0, 1], [1, 0, 0, 0]],
+                             [[0, 1, 0, 0], [0, 0, 1, 0], [1, 0, 1, 0], [0, 0, 0, 0]],
+                            [[0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]]])
+        res = p.get_derivative()
+        print(res)
+
+        cmp = (res == expected_res)
+        self.assertEqual(cmp.all(), True)
+        
 
 if __name__ == '__main__':
     unittest.main()
