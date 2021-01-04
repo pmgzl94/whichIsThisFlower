a9a296637b61fe212eda0a8c4491c29edada1540
aurele <aurele.auboin@epitech.eu>
2021-01-04 15:54:32 +0100

    [Add]: tmp branch

--- client/lib/takePicture.dart
+++ client/lib/takePicture.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-final String takePic = """
+final String takePicture = """
   mutation logout(\$token: String!) {
       logout(token: \$token) {
           ok {
@@ -12,6 +12,15 @@ final String takePic = """
       }
   }
 """;
+
+// onPressed: () async {
+//   var imgFile = await ImagePicker.pickImage(
+//     source: ImageSource.camera
+//   );
+//   setState((){
+//     imgs.add(Image.file(imgFile));
+//   });
+// }
 
 class CreateTakePictureButton extends StatefulWidget
 {
@@ -51,7 +60,7 @@ class CreateTakePictureButtonState extends State<CreateTakePictureButton>
               children: <Widget> [
                Mutation(
                   options: MutationOptions(
-                    documentNode: gql(takePic),
+                    documentNode: gql(takePicture),
                     update: (Cache cache, QueryResult result) {
                       return cache;
                     },
@@ -85,4 +94,273 @@ class CreateTakePictureButtonState extends State<CreateTakePictureButton>
             )
           );
     }
+}
+
+AlertDialog dialog(context, mssg) {
+  return AlertDialog(
+    title: Text('Error occured'),
+    content: SingleChildScrollView(
+        child: ListBody(
+            children: <Widget>[
+                Text(mssg),
+            ],
+        ),
+    ),
+    actions: <Widget>[
+        TextButton(
+            child: Text("Close"),
+            onPressed: () {
+                Navigator.of(context).pop();
+            },
+        ),
+     ],
+  );
+}
+
+class CreateTakePicture extends StatefulWidget
+{
+  @override
+  CreateTakePictureState createState() => CreateTakePictureState();
+}
+
+class CreateTakePictureState extends State<CreateTakePicture>
+{
+    final _id = GlobalKey<FormState>();
+    bool state = false;
+
+    void hasClicked()
+    {
+      this.state = true;
+    }
+
+    final mc1 = TextEditingController();
+    final mc2 = TextEditingController();
+
+    @override
+    void dispose() {
+    // Clean up the controller when the widget is disposed.
+      mc1.dispose();
+      mc2.dispose();
+      super.dispose();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Form(
+            key: _id,
+            child: Column(
+              children: <Widget> [
+                Mutation(
+                  options: MutationOptions(
+                    documentNode: gql(takePicture),
+                    update: (Cache cache, QueryResult result) {
+                      return cache;
+                    },
+                    onError: (result) {
+                      print("error");
+                      print(result);
+                    },
+                    onCompleted: (dynamic resultData) {
+                      print("on completed");
+                      print(resultData.data);
+                      print("takePICTURE");
+                      // Navigator.pop(context);
+                      // if (resultData != null) {
+                      //   print(resultData.data["auth"]["accessToken"]);
+                      //   Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(builder: (context) => CreateMenu(token: resultData.data["auth"]["accessToken"])),
+                      //   );
+                      // } else {
+                      //   print("User doesn't exist");
+                      //   showDialog<AlertDialog>(
+                      //     context: context,
+                      //     builder: (BuildContext context) {
+                      //       return dialog(context, "User doesn't exist");
+                      //   });
+                      // }
+                    }
+                  ),
+                  builder: (RunMutation runMutation, QueryResult result) {
+                    return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            print("GET TOKENNNNNNN");
+                            print(widget.token);
+                            print("GET ENDED");
+                            runMutation({"token": widget.token});
+                          },
+                          child: Text('Take Picture'),
+                        )
+                    );
+                  }
+                )
+              ]
+            )
+          );
+    }
+}
+
+class ButtonCreateTakePicture extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+      return ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CreateTakePicture()),
+                );
+              },
+              child: Text('TakePicture'),
+            );
+  }
+}
+
+
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
+
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      // Wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner
+      // until the controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved using the
+            // pattern package.
+            final path = join(
+              // Store the picture in the temp directory.
+              // Find the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved.
+            await _controller.takePicture(path);
+
+            // If the picture was taken, display it on a new screen.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
+    );
+  }
 }
