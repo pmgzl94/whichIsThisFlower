3d57728c46827e201edb9af398e04c685584c7b9
gonzalo <pierremickael.gonzalo@gmail.com>
2021-01-10 18:46:23 +0100

    [Add]: models zf5, with layer reloading

--- server/cnn/models.py
+++ server/cnn/models.py
@@ -22,23 +22,58 @@ def zf5model(path=example1):
     # image = iml.ImageLoader.getOutputNpArray(rp_dataset + "daisy/" + "5547758_eea9edfd54_n.jpg", gray=True)
     # image = iml.ImageLoader.getCropedImage(rp_dataset + "daisy/" + "5547758_eea9edfd54_n.jpg")#.getOutputNpArray(rp_dataset + "daisy/" + "5547758_eea9edfd54_n.jpg")
 
+    #it crops by 224x224 by default
     input = iml.ImageLoader.getOutputNpArray(path, crop=True)
 
-
     layerContainer = [
-        conv.ConvLayer(padding=1, filtershape=(96, 3, 7, 7), stride_length=2, pool=pool.PoolLayer(pad=1, pool_size=(3, 3), stride_length=2)),
-        conv.ConvLayer(filtershape=(256, 96, 5, 5), stride_length=2, pool=pool.PoolLayer(pad=1, pool_size=(3, 3), stride_length=2)),
+        #3, 224, 224
+        conv.ConvLayer(padding=1, filtershape=(96, 3, 7, 7), stride_length=2, pool=pool.PoolLayer(pad=1, pool_size=(3, 3), stride_length=2), ishape=(3, 224, 224)),
+        #96, 55, 55
+        conv.ConvLayer(filtershape=(256, 96, 5, 5), stride_length=2, pool=pool.PoolLayer(pad=1, pool_size=(3, 3), stride_length=2), ishape=(96, 55, 55)),
+        #256, 26, 26
+        conv.ConvLayer(padding=1, filtershape=(384, 256, 3, 3), stride_length=1, ishape=(256, 26, 26)),
         
-        conv.ConvLayer(padding=1, filtershape=(384, 256, 3, 3), stride_length=1),
-        conv.ConvLayer(padding=1, filtershape=(384, 384, 3, 3), stride_length=1),
+        #384, 13, 13
+        conv.ConvLayer(padding=1, filtershape=(384, 384, 3, 3), stride_length=1, ishape=(384, 13, 13)),
 
+        #384, 13, 13
+        conv.ConvLayer(padding=1, filtershape=(256, 384, 3, 3), stride_length=1, pool=pool.PoolLayer(pool_size=(3, 3), stride_length=2), ishape=(384, 13, 13)),
 
-        conv.ConvLayer(padding=1, filtershape=(256, 384, 3, 3), stride_length=1, pool=pool.PoolLayer(pool_size=(3, 3), stride_length=2)),
-
-        fcnetwork.FCLayer(arch=[9216, 4096, 5])
+        #do use he initialization here
+        fcnetwork.FCLayer(arch=[9216, 4096, 4096, 5])
     ]
 
     zf5 = model.Model(learning_rate=None, dataset=None, layerContainer=layerContainer)
+
+    output = zf5.compute(input)
+
+    print(f"first output = {output}")
+
+    zf5.saveLayers(["conv1", "conv2", "conv3", "conv4", "conv5", "classifier"])
+
+    layerContainer = [
+        #3, 224, 224
+        conv.ConvLayer(load_path="conv1"),
+        #96, 55, 55
+        conv.ConvLayer(load_path="conv2"),
+        #256, 26, 26
+        conv.ConvLayer(load_path="conv3"),
+        
+        #384, 13, 13
+        conv.ConvLayer(load_path="conv4"),
+
+        #384, 13, 13
+        conv.ConvLayer(load_path="conv5"),
+
+        #do use he initialization here
+        fcnetwork.FCLayer(arch=[9216, 4096, 4096, 5], load_path="classifier")
+    ]
+
+    zf5 = model.Model(learning_rate=None, dataset=None, layerContainer=layerContainer)
+
+    output = zf5.compute(input)
+
+    print(f"snd output = {output}")
 
     try:
         output = zf5.compute(input)
@@ -47,3 +82,4 @@ def zf5model(path=example1):
         return "error"
     return return_response(output)
 
+print(zf5model())
