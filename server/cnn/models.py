5c961d2cd8e33a622173a145bacef417ec0cb0cf
gonzalo <pierremickael.gonzalo@gmail.com>
2021-01-16 17:58:39 +0100

    [Add]: flower and fun model from kaggle learning not started

--- server/cnn/models.py
+++ server/cnn/models.py
@@ -15,6 +15,70 @@ def return_response(res):
     idx = res.index(max(res))
     return labels[idx]
 
+def overfeat(path=example1):
+#     input = iml.ImageLoader.getOutputNpArray(path, crop=True)
+
+    layerContainer = [
+        #3, 224, 224
+        conv.ConvLayer(padding=1, filtershape=(96, 3, 7, 7), stride_length=2, pool=pool.PoolLayer(pool_size=(3, 3), stride_length=3), ishape=(3, 224, 224)),
+        #96, 36, 36
+        conv.ConvLayer(padding=1, filtershape=(256, 96, 5, 5), stride_length=1, pool=pool.PoolLayer(pad=1, pool_size=(2, 2), stride_length=2), ishape=(96, 36, 36)),
+        #256, 18, 18
+        conv.ConvLayer(padding=1, filtershape=(512, 256, 3, 3), stride_length=1, ishape=(256, 18, 18)),
+        #512, 18, 18
+        conv.ConvLayer(padding=1, filtershape=(512, 512, 3, 3), stride_length=1, ishape=(256, 18, 18)),
+        #512, 18, 18
+        conv.ConvLayer(padding=1, filtershape=(512, 512, 3, 3), stride_length=1, ishape=(256, 18, 18)),
+        #512, 18, 18
+        conv.ConvLayer(padding=1, filtershape=(512, 512, 3, 3), stride_length=1, ishape=(256, 18, 18)),
+        #512, 18, 18
+        conv.ConvLayer(padding=1, filtershape=(512, 512, 3, 3), stride_length=1, ishape=(256, 18, 18)),
+    ]
+
+    # model.learn()
+
+    overfeat = model.Model(learning_rate=None, dataset=None, layerContainer=layerContainer)
+    output = overfeat.compute(input)
+
+    #     overfeat.saveLayers(["OVconv1", "OVconv2", "OVconv3", "OVconv4", "OVconv5", "OVconv6", "OVconv7", "OVclassifier"])
+
+    overfeat.learn()
+
+# def AlexNetFast
+    #ouput is the probleme the weights initialization is too long
+
+
+#from: https://www.kaggle.com/sachinsharma1123/flowers-fun
+def flowerAndFun(path=example1):
+    input = iml.ImageLoader.getOutputNpArray(path, crop=True, crop_size=(0, 0, 150, 150))
+
+    layerContainer = [
+        #3, 150, 150
+        conv.ConvLayer(filtershape=(32, 3, 3, 3), stride_length=1, pool=pool.PoolLayer(pool_size=(2, 2), stride_length=2), ishape=(3, 150, 150)),
+        
+        #32, 74, 74
+        conv.ConvLayer(filtershape=(64, 32, 3, 3), stride_length=1, pool=pool.PoolLayer(pool_size=(2, 2), stride_length=2), ishape=(32, 74, 74)),
+      
+        #64, 36, 36
+        conv.ConvLayer(filtershape=(128, 64, 3, 3), stride_length=1, pool=pool.PoolLayer(pool_size=(2, 2), stride_length=2), ishape=(64, 36, 36)),
+        
+        #128, 17, 17
+        fcnetwork.FCLayer(arch=[36992, 512, 128, 5])
+    ]
+
+    model_FAndF = model.Model(learning_rate=0.001, dataset=None, layerContainer=layerContainer)
+
+    output = model_FAndF.compute(input, learn=True)
+
+    print(f"output = {output}")
+
+
+
+
+
+
+flowerAndFun()
+
 def zf5model(path=example1):
     # from this architecture: 
     # https://www.researchgate.net/figure/Architecture-of-ZF-model-An-3-channels-image-with-224224-is-as-the-input-It-is_fig5_318577329
@@ -25,31 +89,31 @@ def zf5model(path=example1):
     #it crops by 224x224 by default
     input = iml.ImageLoader.getOutputNpArray(path, crop=True)
 
-    layerContainer = [
-        #3, 224, 224
-        conv.ConvLayer(padding=1, filtershape=(96, 3, 7, 7), stride_length=2, pool=pool.PoolLayer(pad=1, pool_size=(3, 3), stride_length=2), ishape=(3, 224, 224)),
-        #96, 55, 55
-        conv.ConvLayer(filtershape=(256, 96, 5, 5), stride_length=2, pool=pool.PoolLayer(pad=1, pool_size=(3, 3), stride_length=2), ishape=(96, 55, 55)),
-        #256, 26, 26
-        conv.ConvLayer(padding=1, filtershape=(384, 256, 3, 3), stride_length=1, ishape=(256, 26, 26)),
+    # layerContainer = [
+    #     #3, 224, 224
+    #     conv.ConvLayer(padding=1, filtershape=(96, 3, 7, 7), stride_length=2, pool=pool.PoolLayer(pad=1, pool_size=(3, 3), stride_length=2), ishape=(3, 224, 224)),
+    #     #96, 55, 55
+    #     conv.ConvLayer(filtershape=(256, 96, 5, 5), stride_length=2, pool=pool.PoolLayer(pad=1, pool_size=(3, 3), stride_length=2), ishape=(96, 55, 55)),
+    #     #256, 26, 26
+    #     conv.ConvLayer(padding=1, filtershape=(384, 256, 3, 3), stride_length=1, ishape=(256, 26, 26)),
         
-        #384, 13, 13
-        conv.ConvLayer(padding=1, filtershape=(384, 384, 3, 3), stride_length=1, ishape=(384, 13, 13)),
+    #     #384, 13, 13
+    #     conv.ConvLayer(padding=1, filtershape=(384, 384, 3, 3), stride_length=1, ishape=(384, 13, 13)),
 
-        #384, 13, 13
-        conv.ConvLayer(padding=1, filtershape=(256, 384, 3, 3), stride_length=1, pool=pool.PoolLayer(pool_size=(3, 3), stride_length=2), ishape=(384, 13, 13)),
+    #     #384, 13, 13
+    #     conv.ConvLayer(padding=1, filtershape=(256, 384, 3, 3), stride_length=1, pool=pool.PoolLayer(pool_size=(3, 3), stride_length=2), ishape=(384, 13, 13)),
 
-        #do use he initialization here
-        fcnetwork.FCLayer(arch=[9216, 4096, 4096, 5])
-    ]
+    #     #do use he initialization here
+    #     fcnetwork.FCLayer(arch=[9216, 4096, 4096, 5])
+    # ]
 
-    zf5 = model.Model(learning_rate=None, dataset=None, layerContainer=layerContainer)
+    # zf5 = model.Model(learning_rate=None, dataset=None, layerContainer=layerContainer)
 
-    output = zf5.compute(input)
+    # output = zf5.compute(input)
 
-    print(f"first output = {output}")
+    # print(f"first output = {output}")
 
-    zf5.saveLayers(["conv1", "conv2", "conv3", "conv4", "conv5", "classifier"])
+    # zf5.saveLayers(["conv1", "conv2", "conv3", "conv4", "conv5", "classifier"])
 
     layerContainer = [
         #3, 224, 224
@@ -71,9 +135,9 @@ def zf5model(path=example1):
 
     zf5 = model.Model(learning_rate=None, dataset=None, layerContainer=layerContainer)
 
-    output = zf5.compute(input)
-
-    print(f"snd output = {output}")
+    # output = zf5.compute(input)
+    # res = zf5.learn()
+    # print(f"snd output = {res}")
 
     try:
         output = zf5.compute(input)
@@ -82,4 +146,12 @@ def zf5model(path=example1):
         return "error"
     return return_response(output)
 
-print(zf5model())
+# print(zf5model())
+# overfeat()
+# a = numpy.array(
+#         [[1, 2, 3, 4, 5, 6],
+#         [1, 2, 3, 4, 5, 6],
+#         [1, 2, 3, 4, 5, 6],
+#         [1, 2, 3, 4, 5, 6],
+#         [1, 2, 3, 4, 5, 6],
+#         [1, 2, 3, 4, 5, 6]])
