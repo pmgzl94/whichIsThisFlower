5ebd94f8fd102f05fc42af6c1b14cc42cba269fb
gonzalo <pierremickael.gonzalo@gmail.com>
2021-01-22 18:44:27 +0100

    [Add]: add stride length getter; max pool overlaping function aborted: found no way to track the gradient

--- server/cnn/pool.py
+++ server/cnn/pool.py
@@ -3,19 +3,77 @@ import numpy
 
 # def max_pool_derivative(input):
 
+
+## way to compute delta with derivative 
+## for pool overlap used in zf5 and alex net model
+def reshape_delta_and_compute(derivative, delta, pool_size, stride):
+    # for 3d
+    
+    # return derivative
+
+    # reshape delta
+    # shape delta = 256, 6, 6
+    reshaped_delta = delta.reshape(*delta.shape, 1, 1)
+    before_last, last = (len(reshaped_delta.shape) - 2, len(reshaped_delta.shape) - 1)
+
+
+    reshaped_delta = reshaped_delta.repeat(pool_size[-2], axis=before_last).repeat(pool_size[-1], axis=last)
+
+    #derivative resized with 
+    #shape 256, 13, 13
+    
+    #init output shape
+    tderivative = derivative.T
+    part1 = tderivative.shape # 13, 13, 256
+    part2 = (delta.shape[-2], delta.shape[-1]) # 6, 6
+    part3 = pool_size # 3, 3
+
+    outputshape = (*part1, *part2, *part3)
+    
+    #init stride output shape
+    (s1, s2, s3) = tderivative.strides
+
+    # 13, 13, 256
+    part1 = (s1, s2, s3)
+    # 6, 6
+    part2 = (s2*stride, s1*stride) #x and y are reversed because derivative is transposed
+    # 3, 3
+    part3 = s2, s1 #x and y are reversed because derivative is transposed
+    outputstride = (*part1, *part2, *part3)
+
+    restride_derivative = numpy.lib.stride_tricks.as_strided(tderivative, shape=outputshape, strides=outputstride)
+
+    output = restride_derivative * reshaped_delta
+    # print(f"delta * max_derivative = {output.shape}")
+    # print(f"restride derivative shape = {restride_derivative.shape}")
+
+    # print(f"restride dtype = {restride_derivative.dtype}")
+    # print(f"output dtype = {output.dtype}")
+    numpy.copyto(restride_derivative, output)
+
+    #print(f"after computed derivative = {numpy.ma.is_masked(restride_derivative)}")
+
+    computed_derivative = numpy.lib.stride_tricks.as_strided(restride_derivative, shape=tderivative.shape, strides=tderivative.strides)
+    
+    #print(f"after computed derivative = {numpy.ma.is_masked(computed_derivative)}")
+
+    return (computed_derivative.T)
+
 def helper_pool_backprop(delta, pool_layer):
     derivative = pool_layer.get_derivative()
     pool_size = pool_layer.get_pool_size()
-    
-    # if len(delta.shape) > 1:
-    #     return delta * derivative
-    # else:
-    # delta = delta.reshape(pool_size)
-    before_last, last = (len(delta.shape) - 2, len(delta.shape) - 1)
+    stride = pool_layer.get_stride_length()
 
-    delta = delta.repeat(pool_size[-2], axis=before_last).repeat(pool_size[-1], axis=last)
+    # print(f"delta shape = {delta.shape}")
+    # print(f"derivative = {derivative[0]}")
+    ## choose the last two axis
+    before_last, last = (len(delta.shape) - 2, len(delta.shape) - 1)
     
+    ## reshape delta
+    delta = delta.repeat(pool_size[-2], axis=before_last).repeat(pool_size[-1], axis=last)
+
     return delta * derivative
+    # return reshape_delta_and_compute(derivative, delta, pool_size, stride)
 
 class PoolLayer(LayerInterface):
     # if you precise padding, you will only get a padding of 1
@@ -77,7 +135,7 @@ class PoolLayer(LayerInterface):
         outputstride = [s*stride_input[-2], s*stride_input[-1]]
 
         if len(input.shape) == 3:
-            #add third dim
+            # add third dim
             outputshape.append(input.shape[0])
             outputstride.append(stride_input[0])
 
@@ -93,7 +151,7 @@ class PoolLayer(LayerInterface):
             
             output = numpy.amax(output, axis=(3, 4), keepdims=True)
 
-
+            # print(f"output strid shape = {output.shape}")
             
             output = output.reshape(outputshape[:3])
             output = output.transpose(2, 0, 1)
@@ -129,6 +187,8 @@ class PoolLayer(LayerInterface):
         return self.derivative
     def get_pool_size(self):
         return self.pool_size
+    def get_stride_length(self):
+        return self.stride_len
 
 def SPPLayer(LayerInterface):
     def __init__(self, nb_level=3):
